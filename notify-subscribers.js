4f4aceb4b6617cd6bb3b2fe5306d5423d59c9c21
ejnshtein <ejnshtein@gmail.com>
2020-03-05 22:55:27 +0200

    fixed subscriptions

--- notify-subscribers.js
+++ notify-subscribers.js
@@ -6,6 +6,7 @@ import buttons from './lib/buttons.js'
 import getXtFromMagnet from './lib/get-torrent-hash-from-magnet.js'
 import HtmlEntities from 'html-entities'
 import { templates } from './lib/templates.js'
+import buffer from './lib/buffer.js'
 const { AllHtmlEntities } = HtmlEntities
 const { decode } = new AllHtmlEntities()
 
@@ -27,7 +28,7 @@ export async function notifyUsers (torrent, skip = 0) {
 async function findValidRegex (subscriptions, torrent) {
   for (const sub of subscriptions) {
     // console.log(sub, torrent.name, torrent.submitter)
-    if (!sub.users || sub.users.length === 0) {
+    if (!sub.chats || sub.chats.length === 0) {
       continue
     }
     if (sub.conditions) {
@@ -56,35 +57,9 @@ async function findValidRegex (subscriptions, torrent) {
       }
     }
     return sendMessages(
-      sub.users,
-      formatMessage(torrent),
-      {
-        parse_mode: 'HTML',
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: buttons.torrent.download,
-                callback_data: `d=${torrent.id}`
-              },
-              {
-                text: buttons.torrent.magnet,
-                url: `${env.MAGNET_REDIRECT_HOST}/${env.MAGNET_REDIRECT_PREFIX}/${getXtFromMagnet(torrent.magnet)}`
-              },
-              {
-                text: buttons.share,
-                switch_inline_query: `torrent:${torrent.id}`
-              }
-            ],
-            // [
-            //   {
-            //     text: 'Unsubscribe',
-            //     callback_data: `subscribe:id=${sub._id}&s=1&subscribed=1`
-            //   }
-            // ]
-          ]
-        }
-      }
+      torrent,
+      sub.chats,
+      formatMessage(torrent)
     )
   }
 }
@@ -99,16 +74,50 @@ New torrent just arrived!
 .replace(/&/gi, '&amp;')}</a>
 <b>Submitter:</b> ${torrent.submitter !== 'Anonymous' ? `<a href="https://${env.HOST}/user/${torrent.submitter}">${torrent.submitter}</a>` : torrent.submitter}
 <b>Size:</b> ${(torrent.filesize / 1000000000).toFixed(3)} GiB
-<b>Creation Date:</b>: ${templates.date(torrent.creation_date)}
+<b>Creation Date:</b>: ${templates.date(new Date(torrent.creation_date))}
 <b>Hash</b>: ${torrent.hash_hex}
 `
 }
 
-async function sendMessages (users = [], text, extra) {
+async function sendMessages (torrent, chats = [], text) {
   const messages = []
-  for (const userId of users) {
+  for (const chatId of chats) {
+    const keyboard = [
+      {
+        text: buttons.torrent.magnet,
+        url: `${env.MAGNET_REDIRECT_HOST}/${env.MAGNET_REDIRECT_PREFIX}/${getXtFromMagnet(torrent.magnet)}`
+      },
+      {
+        text: buttons.share,
+        switch_inline_query: `torrent:${torrent.id}`
+      }
+    ]
+    if (chatId > 0) {
+      keyboard.unshift(
+        {
+          text: buttons.torrent.download,
+          callback_data: `d=${torrent.id}`
+        }
+      )
+    } else {
+      keyboard.unshift(
+        {
+          text: buttons.torrent.download,
+          url: `https://t.me/${env.BOT_USERNAME}?start=${buffer.encode(`download:${torrent.id}`)}`
+        }
+      )
+    }
     try {
-      messages.push(await telegram.sendMessage(userId, text, extra))
+      messages.push(
+        await telegram.sendMessage(chatId, text,
+          {
+            parse_mode: 'HTML',
+            reply_markup: {
+              inline_keyboard: keyboard
+            }
+          }
+        )
+      )
     } catch (e) {
       console.log('error', e)
     }
