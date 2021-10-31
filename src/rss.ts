e020717541b9daba16c02d7609b6c576943fb675
Okabe Rintaro <ejnshtein@gmail.com>
2021-10-31 22:54:15 +0200

    fix

--- src/rss.ts
+++ src/rss.ts
@@ -2,5 +2,5 @@ import { NyaaRss } from '@ejnshtein/nyaasi'
 import { getEnv } from './lib/get-env'
 
 export const rss = new NyaaRss({
-  host: `https://${getEnv('REAL_HOST')}`
+  host: `https://${getEnv('REAL_HOST')}/`
 })
