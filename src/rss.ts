cd275370b9d9c110108eb094041791f130d270d7
Okabe Rintaro <ejnshtein@gmail.com>
2021-10-31 22:53:19 +0200

    use REAL_HOST for rss host

--- src/rss.ts
+++ src/rss.ts
@@ -2,5 +2,5 @@ import { NyaaRss } from '@ejnshtein/nyaasi'
 import { getEnv } from './lib/get-env'
 
 export const rss = new NyaaRss({
-  host: `https://${getEnv('MAGNET_REDIRECT_HOST')}/nyaasi/`
+  host: `https://${getEnv('REAL_HOST')}`
 })
